7fe9fec15a7d59ad09d765a2767fee971e1c3e09
TzuChingg <suger5769@gmail.com>
2024-01-22 17:18:36 +0800

    123

--- products.js
+++ products.js
@@ -1,14 +1,15 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 
 let productModal = null;
-
+let delProductModal = null;
 const app = {
     data() {
         return {
             api_Url: 'https://ec-course-api.hexschool.io/v2',
             api_Path: 'chinging',
             products: [], 
-            modalProduct: ''
+            modalProduct: '',
+            newModalProduct: ''
         }
     },
     methods: {
@@ -32,17 +33,38 @@ const app = {
                     console.log(err);
                 });
         },
-        openModal(item){
-            console.log(item);
-            this.modalProduct = item
-            productModal.show()
+        openModal(state, item){
+            if (state === 'isNew'){
+                this.modalProduct = ''
+                productModal.show()
+            }else if (state === 'edit'){
+                this.modalProduct = item
+                productModal.show()
+            }else if (state === 'del'){
+                this.modalProduct = item
+                delProductModal.show()
+            }
+        },
+        updateProduct(state, id){
+            if (state === 'isNew'){
+                
+                // axios.post(`${this.api_Url}/v2/api/${this.api_Path}/admin/product`, {data: temp})
+            
+            }else if (state === 'edit'){
+                this.modalProduct = item
+                productModal.show()
+            }else if (state === 'del'){
+                this.modalProduct = item
+                delProductModal.show()
+            }
+
         }
         
     },
     mounted() {
         axios.defaults.headers.common['Authorization'] = document.cookie.split('=')[1]
         productModal = new bootstrap.Modal(document.getElementById('editProduct'))
-        console.log(productModal);
+        delProductModal = new bootstrap.Modal(document.getElementById('delProduct'))
         this.checkUser()
     },
 }
